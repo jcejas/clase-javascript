c21dc27550fd604f33fc67851543034c0218d56b
jcejas <ciglesias@bigtree.com.ar>
2021-04-21 22:42:17 -0300

    First Version

--- script.js
+++ script.js
@@ -2,21 +2,12 @@
 
 function AddClass(){
 	console.log("Boton ADD");
-	var titulo = document.querySelector("h1");
-
-	titulo.classList.add("title");
 }
 
 function RemoveClass(){
 	console.log("Boton REMOVE");
-	var titulo = document.querySelector("h1");
-
-	titulo.classList.remove("title");
 }
 
 function ToggleClass(){
 	console.log("Boton TOGGLE");
-	var titulo = document.querySelector("h1");
-
-	titulo.classList.toggle("title");
 }
